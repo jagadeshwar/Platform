fc80d0a16ee1326aa84b1b840d8be1a563f2e9fd
jagadeshwar <jagadeeshwar.reddy006@gmail.com>
2018-01-23 00:09:53 +0530

    Working Orchestration-Admin

--- orchestration/orchestration-admin/src/main/java/com/school/orchestrationadmin/OrchestrationAdminApplication.java
+++ orchestration/orchestration-admin/src/main/java/com/school/orchestrationadmin/OrchestrationAdminApplication.java
@@ -2,8 +2,10 @@ package com.school.orchestrationadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrchestrationAdminApplication {
 
 	public static void main(String[] args) {
