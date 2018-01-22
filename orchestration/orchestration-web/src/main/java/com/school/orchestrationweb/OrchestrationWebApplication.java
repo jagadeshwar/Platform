e2484cc8f1163e87189427d2798d2b9f6d467e91
jagadeshwar <jagadeeshwar.reddy006@gmail.com>
2018-01-23 00:21:34 +0530

    Orchestration-Web

---
+++ orchestration/orchestration-web/src/main/java/com/school/orchestrationweb/OrchestrationWebApplication.java
@@ -0,0 +1,12 @@
+package com.school.orchestrationweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrationWebApplication.class, args);
+	}
+}
