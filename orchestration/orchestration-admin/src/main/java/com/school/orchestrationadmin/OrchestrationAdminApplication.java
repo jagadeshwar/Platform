57592a8f8b647c4b8389305de69fc8698fbf51e3
jagadeshwar <jagadeeshwar.reddy006@gmail.com>
2018-01-22 23:02:24 +0530

    commiting orchestration

---
+++ orchestration/orchestration-admin/src/main/java/com/school/orchestrationadmin/OrchestrationAdminApplication.java
@@ -0,0 +1,12 @@
+package com.school.orchestrationadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrationAdminApplication.class, args);
+	}
+}
