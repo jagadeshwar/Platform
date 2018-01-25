2d23cb9a88e22a64fb400d89d82c460f32907718
jagadeshwar <jagadeeshwar.reddy006@gmail.com>
2018-01-25 08:58:27 +0530

    After creating dependency for orchestration-service

--- orchestration/orchestration-web/src/main/java/com/school/orchestrationweb/OrchestrationWebApplication.java
+++ orchestration/orchestration-web/src/main/java/com/school/orchestrationweb/OrchestrationWebApplication.java
@@ -2,11 +2,14 @@ package com.school.orchestrationweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrchestrationWebApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(OrchestrationWebApplication.class, args);
 	}
 }
